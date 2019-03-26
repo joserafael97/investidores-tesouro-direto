1777ef6dff5dd7a3fb8d1ef353d0b49682b15828
JoseRafael97 <jose.rafael.feitosa@gmail.com>
2019-03-26 00:31:20 -0300

    adição de exemplos e correções nos dados

--- code/import_data.R
+++ code/import_data.R
@@ -4,6 +4,7 @@ library(here)
 
 investidores_tesouro = readr::read_csv2(here::here("data/InvestidoresTesouroDireto.csv"))
 
+
 investidores_ativos_tesouro = investidores_tesouro %>% 
     filter(`Operou 12 Meses` == 'S' ) %>% 
     group_by(`Codigo do Investidor`) %>% 
@@ -12,12 +13,13 @@ investidores_ativos_tesouro = investidores_tesouro %>%
               genero = last(Genero),
               profissao = last(Profissao),
               idade = last(as.numeric(Idade)),
-              estado_civil = last(`Estado Civil`),
+              estado_civil = last(ifelse(grepl('Casado', `Estado Civil`), 'Casado(a)', `Estado Civil`)),
               data_adesao = last(as.Date(`Data de Adesao`, format = "%d/%m/%Y"))
              )
 
-investidores_ativos_tesouro = investidores_ativos_tesouro() %>% 
-    mutate(ano_adesao = floor_date(data_adesao, unit = "year"))
+investidores_ativos_tesouro = 
+    investidores_ativos_tesouro %>% 
+    mutate(ano_adesao = lubridate::year(data_adesao))
 
 investidores_ativos_tesouro %>% 
     write_csv(here::here("dados/investidores_ativos_tesouro.csv"))
